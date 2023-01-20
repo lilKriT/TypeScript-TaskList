e090a5c2e71045c0fe66b0d9ab58a77182fb1add
Piotr Filipiak <kiddhkane@gmail.com>
2023-01-20 20:43:55 +0100

    Working on adding tasks.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,23 @@
-console.log("working")
+console.log("Starting");
 
-export {}
+const addTaskBtn: HTMLButtonElement = document.querySelector(
+  "#addTask"
+) as HTMLButtonElement;
+
+const taskNameInput: HTMLInputElement = document.querySelector(
+  "#taskName"
+) as HTMLInputElement;
+
+addTaskBtn?.addEventListener("click", (e) => {
+  e.preventDefault();
+
+  const taskName = taskNameInput.value;
+
+  console.log(`Current Value: ${taskName}`);
+});
+
+const addTask = () => {
+  console.log("adding task");
+};
+
+export {};
