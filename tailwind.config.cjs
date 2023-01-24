bdbb0a89f2666f41a938a4ae85338b6d707d180b
Piotr Filipiak <kiddhkane@gmail.com>
2023-01-24 20:45:26 +0100

    Added a background.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        neonHighway: "url(/src/assets/neon-highway.webp)",
+      },
+    },
   },
   plugins: [],
 };
