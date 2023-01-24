d01310dd5a7b3996fa6977c595572c073d1d8c87
Piotr Filipiak <kiddhkane@gmail.com>
2023-01-24 22:29:10 +0100

    Working on glassmorphism.

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,10 +4,13 @@ module.exports = {
   theme: {
     extend: {
       colors: {},
+      boxShadow: {
+        glass: "0 5px 30px rgba(0, 0, 0, .1)",
+      },
       backgroundImage: {
         neonHighway: "url(/src/assets/neon-highway.webp)",
-        mainGradient:
-          "linear-gradient(45deg, rgba(255, 255, 255, .2), rgba(255, 255, 255, .1))",
+        glassGradient:
+          "linear-gradient(45deg, rgba(255, 255, 255, .1), rgba(255, 255, 255, .02))",
       },
     },
   },
